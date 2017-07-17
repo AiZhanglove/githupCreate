e5ae1160c72073dcd1dd44a6567449b4c00fb22b
zhangai <zhangai@xiaomi.com>
2017-07-17 15:33:32 +0800

    add

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+/**
+ * Created by ZhangAi on 2017/7/17.
+ */
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+var minify = require('gulp-minify');
+var rename = require('gulp-rename');
+
+gulp.task('default',function(){
+    //console.log('hello world!')
+});
+
+
+
+gulp.task('minjs',function(){
+    return gulp.src('client/js/**/*.js')
+        .pipe(minify())
+        .pipe(rename({suffix:'.min'}))
+        .pipe(gulp.dest('build/js'))
+        .pipe(notify({message:'js is ok'}))
+
+})
+
+//gulp.task('build',['minjs'],function(){
+//
+//})
