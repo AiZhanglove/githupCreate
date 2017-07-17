254e0825b3b86d1179551d46dd7306e3c334e794
zhangai <zhangai@xiaomi.com>
2017-07-17 17:50:33 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,30 @@ var gulp = require('gulp');
 var notify = require('gulp-notify');
 var minify = require('gulp-minify');
 var rename = require('gulp-rename');
+var exec = require('child_process').exec;
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+
+
+
+
+
+gulp.task('sass',function(){
+    return gulp.src('client/css/**/*.scss')
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest('build/css'))
+})
 
 gulp.task('default',function(){
     //console.log('hello world!')
 });
 
+gulp.task('sass',function(){
+    return gulp.src('client/css/**/*.scss')
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest('build/css'))
+        .pipe(notify({message:'sass is ok'}))
+})
 
 
 gulp.task('minjs',function(){
@@ -24,10 +43,17 @@ gulp.task('minjs',function(){
         }))
         //.pipe(rename({suffix:'.min'}))
         .pipe(gulp.dest('build/js'))
-        .pipe(notify({message:'js is ok'}))
+        .pipe(notify({message:'minjs is ok'}))
 
 })
 
-//gulp.task('build',['minjs'],function(){
-//
-//})
+gulp.task('jekyll',function(cb){
+    exec('jekyll build',function(err){
+        if(err) return cb(err);
+        cb();
+    })
+})
+
+gulp.task('build',['minjs'],function(){
+
+})
